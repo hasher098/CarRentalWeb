3a17e5151f247ac430f40a920c5c807077afb0f4
DESKTOP-6B91V5U\tomek <tomasz.krotosz@wp.pl>
2020-10-12 20:27:45 +0200

    dokonalem zmiany takiej i takiej

--- carrentalweb/src/App.js
+++ carrentalweb/src/App.js
@@ -7,7 +7,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>Zmiana22tomek</p>
+        <p>123123123123123</p>
         <a
           className="App-link"
           href="https://reactjs.org"
