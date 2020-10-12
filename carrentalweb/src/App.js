9516120068d30756de9bfd6d30dcb24797b396de
DESKTOP-6B91V5U\tomek <tomasz.krotosz@wp.pl>
2020-10-12 20:30:48 +0200

    asdxd

--- carrentalweb/src/App.js
+++ carrentalweb/src/App.js
@@ -7,7 +7,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>123123123123123</p>
+        <p>wielun</p>
         <a
           className="App-link"
           href="https://reactjs.org"
