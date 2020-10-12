ca85ccf7dc4d14adbb4e42fde748df554522c094
przemek1680 <przemek1680@gmail.com>
2020-10-12 20:16:45 +0200

    Zmianę zrobiłem xD

--- carrentalweb/src/App.js
+++ carrentalweb/src/App.js
@@ -7,7 +7,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>No powinniscie to widziec</p>
+        <p>Zmiana</p>
         <a
           className="App-link"
           href="https://reactjs.org"
