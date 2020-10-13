fc31194a0597f2336183d9810b906539a21bbcc0
przemek1680 <przemek1680@gmail.com>
2020-10-13 21:10:19 +0200

    opis tydzień 1

--- carrentalweb/src/App.js
+++ carrentalweb/src/App.js
@@ -7,7 +7,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>wielu123123123n</p>
+        <p>Wypożyczalnia samochodowa</p>
         <a
           className="App-link"
           href="https://reactjs.org"
