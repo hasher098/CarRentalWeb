d8c65d9a678a32d58377f927e29ae7ec17755206
DESKTOP-6B91V5U\tomek <tomasz.krotosz@wp.pl>
2020-10-24 02:13:23 +0200

    Wstepny formularz na glownej stronie

--- carrentalweb/src/components/titlePart/TitlePart.jsx
+++ carrentalweb/src/components/titlePart/TitlePart.jsx
@@ -1,38 +1,29 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import useStyles from './TitlePartStyles';
 import Grid from '@material-ui/core/Grid';
 import { TextField, Typography } from '@material-ui/core';
-import { listOfCarsRequest } from '../../api/carClient';
+import Button from '@material-ui/core/Button';
 import Autocomplete from '@material-ui/lab/Autocomplete';
+import SearchForm from '../searchForm/SearchForm';
 const TitlePart = () => {
-  const [data, setData] = useState();
   const classes = useStyles();
-  async function fetchData() {
-    const response = await listOfCarsRequest();
-    setData(response.data);
-  }
-  useEffect(() => {
-    fetchData();
-  }, []);
 
   return (
     <Grid container className={classes.background}>
       <Grid container className={classes.bar}>
-        <Grid item xs={12} md={6}>
-          <Grid item className={classes.bgform}></Grid>
-          <Grid item className={classes.searchbtn}>
-            <Autocomplete
-              id="combo-box-demo"
-              options={data}
-              getOptionLabel={(option) => option.brand}
-              style={{ backgroundColor: `white` }}
-              renderInput={(params) => (
-                <TextField {...params} label="Wybierz samochód" variant="outlined" />
-              )}
-            ></Autocomplete>
+        <Grid item xs={12} md={6} className={classes.firstPart}>
+          <Grid item className={classes.searchBox}>
+            <Grid item className={classes.bgform}>
+              <SearchForm></SearchForm>
+            </Grid>
+            <Grid item className={classes.searchbtn}>
+              <Button variant="contained" color="primary" className={classes.margin}>
+                Wyszukaj samochód
+              </Button>
+            </Grid>
           </Grid>
         </Grid>
-        <Grid item className={classes.bar} xs={12} md={6}>
+        <Grid item className={classes.secondBar} xs={12} md={6}>
           <Typography className={classes.title}>
             Wypożyczalnia samochodów w Częstochowie. Z nami dojedziesz tam, gdzie chcesz.
           </Typography>
