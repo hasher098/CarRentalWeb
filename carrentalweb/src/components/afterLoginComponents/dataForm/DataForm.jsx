7d29809f391b5af94d6c6097c5b49ce72b49a2d2
DESKTOP-6B91V5U\tomek <tomasz.krotosz@wp.pl>
2020-11-11 23:36:45 +0100

    no message

--- carrentalweb/src/components/afterLoginComponents/dataForm/DataForm.jsx
+++ carrentalweb/src/components/afterLoginComponents/dataForm/DataForm.jsx
@@ -6,7 +6,7 @@ import FormControlLabel from '@material-ui/core/FormControlLabel';
 import Grid from '@material-ui/core/Grid';
 import { FormControl } from '@material-ui/core';
 import { useForm } from 'react-hook-form';
-import {updateProfileData} from '../../../api/updateProfileClient';
+import { updateProfileData } from '../../../api/updateProfileClient';
 import Button from '@material-ui/core/Button';
 const DataForm = () => {
   const classes = useStyles();
@@ -14,75 +14,71 @@ const DataForm = () => {
     mode: 'onSubmit',
   });
   async function updateData(data) {
-    try{
-      const response = await updateProfileData (data.firstName, data.lastName, data.address, data.idcardnumber, data.pesel)
-    }
-    catch(error){
-
+    try {
+      const response = await updateProfileData(
+        data.firstName,
+        data.lastName,
+        data.address,
+        data.idcardnumber,
+        data.pesel,
+      );
+      console.log('pomagam');
+    } catch (error) {
+      console.log(error);
     }
   }
   return (
     <FormControl onSubmit={handleSubmit(updateData)}>
-    <Grid container spacing={3} className={classes.formcontainer}>
-        <Grid item xs={12} sm={6}>
-          <TextField
-            id="firstName"
-            name="firstName"
-            label="Imie"
-            fullWidth
-            autoComplete="given-name"
-            inputRef={register}
-          />
-        </Grid>
-        <Grid item xs={12} sm={6}>
-          <TextField
-            id="lastName"
-            name="lastName"
-            label="Nazwisko"
-            fullWidth
-            autoComplete="family-name"
-            inputRef={register}
-          />
-        </Grid>
-        <Grid item xs={12}>
-          <TextField
-            id="address"
-            name="address"
-            label="Addres"
-            fullWidth
-            autoComplete="address"
-            inputRef={register}
-          />
-        </Grid>
-        <Grid item xs={12} sm={6}>
-          <TextField 
-          id="idcardnumber" 
-          name="idcardnumber" 
-          label="Nr dowodu" 
-          fullWidth 
-          inputRef={register}
-          />
-        </Grid>
-        <Grid item xs={12} sm={6}>
-          <TextField
-            id="pesel"
-            name="pesel"
-            label="Pesel"
-            fullWidth
-            inputRef={register}
-          />
-        </Grid>
-        <Button
-              type="submit"
+      <form role="form" noValidate>
+        <Grid container spacing={3} className={classes.formcontainer}>
+          <Grid item xs={12} sm={6}>
+            <TextField
+              id="firstName"
+              name="firstName"
+              label="Imie"
               fullWidth
-              variant="contained"
-              color="primary"
-            >
-              Update
-            </Button>
-      </Grid>
-      </FormControl>
-
+              autoComplete="given-name"
+              inputRef={register}
+            />
+          </Grid>
+          <Grid item xs={12} sm={6}>
+            <TextField
+              id="lastName"
+              name="lastName"
+              label="Nazwisko"
+              fullWidth
+              autoComplete="family-name"
+              inputRef={register}
+            />
+          </Grid>
+          <Grid item xs={12}>
+            <TextField
+              id="address"
+              name="address"
+              label="Addres"
+              fullWidth
+              autoComplete="address"
+              inputRef={register}
+            />
+          </Grid>
+          <Grid item xs={12} sm={6}>
+            <TextField
+              id="idcardnumber"
+              name="idcardnumber"
+              label="Nr dowodu"
+              fullWidth
+              inputRef={register}
+            />
+          </Grid>
+          <Grid item xs={12} sm={6}>
+            <TextField id="pesel" name="pesel" label="Pesel" fullWidth inputRef={register} />
+          </Grid>
+          <Button type="submit" fullWidth variant="contained" color="primary">
+            Update
+          </Button>
+        </Grid>
+      </form>
+    </FormControl>
   );
 };
 
